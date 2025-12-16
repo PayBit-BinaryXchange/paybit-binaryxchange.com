d0581dc1dbcd720da61faa87d8f147c19524b8ae
PayBit-BinaryXchange <paybitbinaryxchange@gmail.com>
2025-12-16 16:19:48 +0100

    Use Atlas MongoDB

--- server.js
+++ server.js
@@ -31,10 +31,13 @@ app.use(flash());
 //app.set('view engine', 'html');
 
 
-// MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/users")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error(err));
+
 
 // Middleware
 
